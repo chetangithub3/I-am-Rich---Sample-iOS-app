28d9b21f27b9b4c0a4bddd97fedc260fac2dcb1b
Chetan <chetan.getsmail@gmail.com>
2022-02-18 17:32:04 -0500

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Chetan Dhowlaghar on 2/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
